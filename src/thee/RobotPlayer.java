28e629ee6b6463575ba80f88698c2429d13372d8
Vixrlie <97731041+Vixrlie@users.noreply.github.com>
2026-03-15 18:36:20 +0700

    feat: main robot player

---
+++ src/thee/RobotPlayer.java
@@ -0,0 +1,55 @@
+package thee;
+
+import battlecode.common.Clock;
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+import battlecode.common.UnitType;
+import static victim.Shared.*;
+
+public class RobotPlayer {
+
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) throws GameActionException {
+        System.out.println("I'm alive");
+        rc.setIndicatorString("Hello world!");
+
+        mapWidth = rc.getMapWidth();
+        mapHeight = rc.getMapHeight();
+        defaultLoc = pickRandomDefaultLoc(rc);
+        target = defaultLoc;
+
+        // Record spawn tower: on first turn, the nearest allied tower is the one that spawned us
+        RobotInfo[] nearby = rc.senseNearbyRobots(-1, rc.getTeam());
+        for (RobotInfo r : nearby) {
+            if (r.getType().isTowerType()) {
+                spawnTower = r.getLocation();
+                break;
+            }
+        }
+
+        if (rc.getType() == UnitType.MOPPER) {
+            isMessenger = true;
+        }
+
+        while (true) {
+            turnCount += 1;
+            try {
+                switch (rc.getType()) {
+                    case SOLDIER:  SoldierPlayer.runSoldier(rc);  break;
+                    case MOPPER:   MopperPlayer.runMopper(rc);    break;
+                    case SPLASHER: break;
+                    default:       TowerPlayer.runTower(rc);      break;
+                }
+            } catch (GameActionException e) {
+                System.out.println("GameActionException");
+                e.printStackTrace();
+            } catch (Exception e) {
+                System.out.println("Exception");
+                e.printStackTrace();
+            } finally {
+                Clock.yield();
+            }
+        }
+    }
+}
